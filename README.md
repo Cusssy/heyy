9985bcee93081beeadb8cfbd669c063e5f336b20
Cusssy <71029516+Cusssy@users.noreply.github.com>
2023-06-22 22:34:33 +0300

    Nueva Paginaweb

--- README.md
+++
@@ -1,4 +0,0 @@
-# heyy
-<p align="center">
-  <img src="https://i.imgur.com/0VfW9WF.gif" />
-</p>
--- src/files/main.js
+++ src/files/main.js
@@ -1,89 +1,37 @@
-function menulist() {
-  var enlacesContainer = document.getElementById("enlacesContainer");
+const outputDiv = document.getElementById('output');
+const inputElement = document.getElementById('input');
 
-  // Verificar si el contenedor de enlaces tiene contenido
-  if (enlacesContainer.innerHTML !== "") {
-      enlacesContainer.style.opacity = 0; // Establecer la opacidad en 0
-      setTimeout(function() {
-          enlacesContainer.innerHTML = ""; // Limpiar el contenido después de que la transición haya finalizado
-      }, 300); // Ajusta la duración de la transición (en milisegundos) según tus necesidades
-  } else {
+function processCommand(command) {
+command = command.trim();
 
-      var enlaces = [
-          { texto: "Twitter cutter", url: "https://cutter.cusssy.com" },
-          { texto: "WordFinder", url: "https://github.com/cusssy/WordFinder" },
-
-          // Agrega más enlaces según sea necesario
-      ];
-
-      enlaces.forEach(function(enlace) {
-          var etiquetaA = document.createElement("a");
-          etiquetaA.href = enlace.url;
-          etiquetaA.textContent = enlace.texto;
-          etiquetaA.id = "lista"; // Agregar esta línea para asignar la ID "lista"
-          enlacesContainer.appendChild(etiquetaA);
-          enlacesContainer.appendChild(document.createElement("br"));
-      });
-
-      // Aplicar una pequeña demora antes de establecer la opacidad en 1 para que la transición se active correctamente
-      setTimeout(function() {
-          enlacesContainer.style.opacity = 1;
-      }, 10);
-  }
+if (command === 'help') {
+    outputDiv.innerHTML += '<br>Lista de comandos <br> -   info: mis cosas <br> -  proyectos: lista de mis proyectos <br> -    heyy: heyy <br> -    clear limpiar terminal<br><br>';
+} else if (command === 'mycommand') {
+    outputDiv.innerHTML += 'Este es mi propio comando!<br>';
+} else if (command === 'clear') {
+    outputDiv.innerHTML = ''; // Limpia la salida
+} else if (command === 'exit') {
+    outputDiv.innerHTML += 'Bye bye!<br>';
+} else if (command === 'info') {
+    outputDiv.innerHTML += "Version 1.0.0<br> Twitter: <a href='https://twitter.com/cusssy_'>cusssy_</a><br>discord: cusssy<br> Github: <a href='https://github.com/Cusssy'>Cusssy</a><br>";
+} else if (command === 'proyectos') {
+    outputDiv.innerHTML += 'Lista de mis proyectos<br><br> -    Tweet cutter<br><br>Cree esta pagina web usando ChatGPT para poder ver todo el potencia que puede dar esta IA, puedo asegurar que no habre editado mas de 20 lineas de codigo en total, 99% lo ha hecho la IA, asique nada mas este proyecto se va a quedar aqui pero no creo que le haga muchos cambios en un futuro<br><br> URL: <a href="https://cutter.cusssy.com">Tweet Cutter</a><br><br> -    WordFinder<br><br>Un sencillo programa que usa la whisper la inteligencia artificia de OpenAI para transcribir todos tus videos/audios a texto el cual sera almacenado en una base de datos de mongoDB.<br><br> -    WordFinder_Twtich<br> <br>Mismo funcionamiento a WordFinder pero para Twitch, descarga todos los clips de un canal, los procesa y los sube a la base de datos de mongoDB.<br><br> Este no esta disponible aun<br><br>';
+} else if (command === 'heyy') {
+    outputDiv.innerHTML += 'Heyy!<br>';
+}
+else {
+    outputDiv.innerHTML += `Command not found: ${command}<br>`;
 }
 
+inputElement.value = '';
+outputDiv.scrollTop = outputDiv.scrollHeight;
+}
 
-
-var ElementosClick = [];
-var contador = 0;
-var audio = new Audio("src/effect.mp3");
-var clickEnabled = true;
-
-document.addEventListener("mousedown", captura_click);
-
-function captura_click(e) {
-  if (!clickEnabled) {
-    return;
-  }
-
-  var HaHechoClick = e.target;
-
-  if (
-    HaHechoClick.tagName.toLowerCase() === "img" &&
-    HaHechoClick.src.toLowerCase().endsWith(".gif")
-  ) {
-    ElementosClick.push(HaHechoClick);
-    contador++;
-
-    if (contador >= 15) {
-      clickEnabled = false;
-      audio.play();
-
-      var overlay = document.createElement("div");
-      overlay.classList.add("overlay");
-
-      var image = document.createElement("img");
-      image.src =
-        "src/nerd.webp";
-      image.classList.add("image");
-
-      overlay.appendChild(image);
-      document.body.appendChild(overlay);
-
-      setTimeout(function () {
-        overlay.classList.add("fadeout");
-        setTimeout(function () {
-          overlay.remove();
-          clickEnabled = true;
-        }, 2000);
-      }, 10000);
-    } else {
-      console.log("Contador:", contador);
+inputElement.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        const command = inputElement.value;
+        outputDiv.innerHTML += `<span class="hostname">guest@cusssy.com</span><span>:</span><span class="hostname2">~</span><span>$</span> ${command} <br>`;
+        processCommand(command);
     }
-  }
-}
+});
 
-// Reiniciar el contador cada 5 segundos
-setInterval(function () {
-  contador = 0;
-}, 5000);
